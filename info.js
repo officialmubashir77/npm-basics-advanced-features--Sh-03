d8e6a75d211bbd553e1b3ab5855e06e6c17bbc9a
officialmubashir77 <officialmubashir77@gmail.com>
2024-07-13 23:01:13 -0700

    npm-basic-and-advanced-featuers

---
+++ info.js
@@ -0,0 +1,34 @@
+// 00:00 Introduction
+// 00:25 About this video
+
+// 00:57 NPM Understanding node package manager
+// 05:22 Installing and uninstalling anything basics & advanced
+// 08:28 Installing particular versions
+// 10:42 Understanding Node modules
+// 13:37 Dependencies & Devdependencies
+// 16:11 Scripts - understanding default scripts PATH and custom scripts 
+
+
+// nodejs core me jo install ho ke ata hy wo kehlta hy module 
+// or ham jo npm se doanload karty hyn wo kehlta hy package
+
+// install => npm i give-me-a-joke
+// uninstall => npm un give-me-a-joke
+// install particular version => npm i give-me-a-joke@3.0.9
+
+
+// var giveMeAJoke = require('give-me-a-joke');
+
+// // To get a random dad joke
+// giveMeAJoke.getRandomDadJoke (function(joke) {
+//     console.log(joke);
+// });
+
+
+// dev_dependcies => esy packages jo sirf development me kam ayengy par jab app ban jayega our upload jo jayeega tab ham in packages ko nhi use kar rhy hongy .
+
+
+// kai bar ap scripts chalaty hoo and usme ap likhty hoo ye command 
+// npm start 
+// npm run dev
+// npm run test 
